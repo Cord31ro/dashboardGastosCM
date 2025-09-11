8d64d7cf45ee71196d2e31a8325882edcc598171
Cord31ro <contapserv@gmail.com>
2025-09-11 12:31:23 -0300

    Nova opção

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ with st.form("novo_gasto"):
         categoria = st.selectbox(
             "Categoria",
             ["Alimentação", "Transporte", "Mão de obra", "Manutenção",
-             "Compra de Materiais", "Combustivel", "Motorista", "Tijolo", "Outro", "Cimento"]
+             "Compra de Materiais", "Combustivel", "Motorista", "Tijolo", "Outro", "Cimento", "Brita"]
         )
     with col2:
         valor = st.number_input("Valor (R$)", min_value=0.01)
